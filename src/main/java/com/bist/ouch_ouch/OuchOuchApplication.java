643761c8e1f54460c44583e9175834f0c58ddb9f
Yusuf Barış Köse <yusufbaris.kose@borsa.local>
2025-06-12 16:55:05 +0300

    initial commit

---
+++ src/main/java/com/bist/ouch_ouch/OuchOuchApplication.java
@@ -0,0 +1,13 @@
+package com.bist.ouch_ouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuchOuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuchOuchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bist/ouch_ouch/OuchOuchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bist.ouch_ouch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuchOuchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
